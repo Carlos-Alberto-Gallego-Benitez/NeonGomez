407e7bc2686b60e2e48c70eac5c23297b46c0995
carlos_benitez <carlosalgabez.08@gmail.com>
2021-05-12 19:10:36 -0500

    .

--- application/view/usuario/login/actualizarcontra/mensaje.php
+++ application/view/usuario/login/actualizarcontra/mensaje.php
@@ -7,7 +7,7 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>Actualizar datos</title>
-        
+            <link rel="icon" type="image/x-icon" href="<?= URL ?>public/img/login/logo.png" />
             <link rel="stylesheet" href="<?= URL ?>public/css/cssrecuperar/estilos.css">
             <link rel="stylesheet" href="<?= URL ?>public/css/cssrecuperar/font-awesome.css">
         
