50b5398b067f190b727049280615ef5d18950c6b
carlos_benitez <carlosalgabez.08@gmail.com>
2021-05-20 17:31:20 -0500

    .

---
+++ application/view/_templates/ayuda.php
@@ -0,0 +1,4 @@
+
+<link href="<?= URL ?>public/css/ayuda/estiloayuda.css" rel="stylesheet" />
+
+<a href="https://chat.whatsapp.com/JaFl53YL0ZfGkicDMomjAb"><h6 class="ayuda text-white mr-5"> ¿Necesitas ayuda?<img src="<?= URL ?>public/img/ayuda/wplog.png" width="40" height="40" ></h6></a>
