af39298775a438a8abf9d736557a24323f0e8aaa
carlos_benitez <carlosalgabez.08@gmail.com>
2021-05-27 16:34:52 -0500

    .

---
+++ application/view/_templates/ayudaonline.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aayuda_online</title>
+    <link rel="icon" type="image/x-icon" href="<?= URL ?>public/img/login/logo.png" />
+    <link href="<?= URL ?>public/css/dashboard/estilos.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body class="bg-black">
+    <h1>ayudaonline</h1>
+    <div class="container contacto">
+  <h2 class="text-center mb-5">¿Cuál es su duda?</h2>
+
+  <div class="row">
+  
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-user-clock icono "></i><h5 class="text-center ">Gestión de usuarios</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+              <a>8:00 a.m a 5:00 p.m lunes a viernes <br>8:00 a.m a 2:00 p.m sábados</a>
+              
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-chalkboard-teacher"></i><h5 class="text-center ">Gestión de proveedores</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+              <a>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   <br>Voluptatibus animi maiores, enim</a>
+              
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-box-tissue"></i><h5 class="text-center ">Gestión de insumos</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+          <a>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   <br>Voluptatibus animi maiores, enim</a>
+              
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-cart-arrow-down"></i><h5 class="text-center ">Gestión de compras</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+          <a>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   <br>Voluptatibus animi maiores, enim</a>
+              
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-child"></i><h5 class="text-center ">Gestión de clientes</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+          <a>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   <br>Voluptatibus animi maiores, enim</a>
+              
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-balance-scale"></i><h5 class="text-center ">Gestión de producto_terminado</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+          <a>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   <br>Voluptatibus animi maiores, enim</a>
+              
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-6 col-md-6">
+      <div class="card bg-light text-dark mb-3">
+          <div class="card-body"><i class="fas fa-box"></i><h5 class="text-center ">Gestión de ventas</h5></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+          <a>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   <br>Voluptatibus animi maiores, enim</a>
+              
+          </div>
+      </div>
+  </div>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/f489c8aeef.js" crossorigin="anonymous"></script>   
+</body>
+</html>
