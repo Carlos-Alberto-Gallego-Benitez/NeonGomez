66dc9831ba1a9e0bcf3c7ad96d5a82dab8742624
Carlos Benítez <carlosalgabez.08@gmail.com>
2023-09-23 14:32:11 -0500

    cambio de crendenciales en bd

--- application/config/config.php
+++ application/config/config.php
@@ -54,8 +54,8 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
  * This is the place where you define your database credentials, database type etc.
  */
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'proyecto');
+define('DB_HOST', 'containers-us-west-194.railway.app');
+define('DB_NAME', 'railway');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'hxkwAXg0jadTb0dTGwhR');
 define('DB_CHARSET', 'utf8');
